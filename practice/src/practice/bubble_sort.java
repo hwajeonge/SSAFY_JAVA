56c0647e9cea91be952b38eed809456f04acd6c0
hwajeonge <wlrmadldi0516@naver.com>
2024-07-30 11:35:41 +0900

    bubble sort

--- practice/src/practice/bubble_sort.java
+++ practice/src/practice/bubble_sort.java
@@ -1,5 +1,23 @@
 package practice;
 
+import java.util.Arrays;
+
 public class bubble_sort {
+	public static void main(String[] args) {
+		int[] arr = {5,2,9,1,5,6};
+		
+		for(int i = arr.length-1; i>0; i--) {
+			for(int j = 0; j<i; j++) {
+				if(arr[j] > arr[j+1]) {
+					int temp = arr[j+1];
+					arr[j+1] = arr[j];
+					arr[j] = temp;
+				}
+			}
+			System.out.println(Arrays.toString(arr));
+		}
+		System.out.println(Arrays.toString(arr));
+		
+	}
 
 }
